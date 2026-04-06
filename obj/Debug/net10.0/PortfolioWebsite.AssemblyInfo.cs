79ea98c78099f152d1fec9963d0b5278b822c65e
Srinivasand04 <Srinivasand04@gmail.com>
2026-04-06 03:12:13 +0200

    Cleaned unnecessary build files and added gitignore

--- obj/Debug/net10.0/PortfolioWebsite.AssemblyInfo.cs
+++ obj/Debug/net10.0/PortfolioWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0af3345332db1bbfe7575cde6e1c56de2958121a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
